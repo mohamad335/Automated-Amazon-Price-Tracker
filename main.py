cde1a9a9951a3ece5f47394310d79cc7f4f4edfe
Mohammad <mhamdl2amen@gmail.com>
2024-08-03 18:32:03 +0300

    add the headers only two

--- main.py
+++ main.py
@@ -2,21 +2,26 @@ from bs4 import BeautifulSoup
 import requests
 from dotenv import load_dotenv
 import os
-import time
 import smtplib
+
 MY_Email=os.getenv("MY_EMAIL")
 My_pass=os.getenv("MY_PASS")
 
 
 to=os.getenv("TO")
 load_dotenv()
-BUY_PRICE=100
-URL="https://appbrewery.github.io/instant_pot/"
-response=requests.get(URL)
-webpage=response.text
-soup=BeautifulSoup(webpage,"html.parser")
-price=soup.find(class_="a-offscreen")
-float_price=float(price.getText().split("$")[1])
+BUY_PRICE=101
+URL="https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
+headrs={
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/000000000 Safari/537.36",
+    "Accept-Language":"en-US,en;q=0.9"
+}
+response=requests.get(URL,headers=headrs)
+
+soup=BeautifulSoup(response.content,"html.parser")
+price=soup.find(class_="aok-offscreen").get_text()
+float_price=float(price.split("$")[1])
+print(float_price)
 if float_price<BUY_PRICE:
     with smtplib.SMTP("smtp.gmail.com",port=587) as connection:
         connection.login(MY_Email, My_pass)
@@ -28,6 +33,16 @@ if float_price<BUY_PRICE:
             msg=f"Subject:Amazon Price Alert!\n\n{float_price}".encode("utf-8")
         )
    
+
+
+
+
+
+
+
+
+
+
   
     
 
